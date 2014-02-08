367a41523273cadb834973f3a91a51a7b604ecb2
4bcxyz <4bcxyz@gmail.com>
2014-02-08 01:57:59 +0100

    Fix bug color in CST

--- public/js/CST.js
+++ public/js/CST.js
@@ -16,7 +16,7 @@ var CST =
    // Color of circle obstacle.
    COL_OBS_CIRC        : 'red',
    // Color of circle obstacle's border.
-   COL_OBS_CIRC_ST     : '663300',
+   COL_OBS_CIRC_ST     : '#663300',
    // Color of players.
    COL_PLAYER          : 'green',
    // Color of a player who has been killed.
